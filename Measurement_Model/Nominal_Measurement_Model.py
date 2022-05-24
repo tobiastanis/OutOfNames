227edbe5dcf0ff055634c2685600896bacb404cc
tobiastanis <t.p.tanis@student.tudelft.nl>
2022-05-24 16:31:01 +0200

    ignore json files, due to size

--- Measurement_Model/Nominal_Measurement_Model.py
+++ Measurement_Model/Nominal_Measurement_Model.py
@@ -5,7 +5,18 @@ import numpy as np
 
 from Initials import Simulation_Time_Setup
 
+add_noise = 0
+add_bias = 0
 
+if add_noise == 1:
+    noise = 20  # This value will differ
+else:
+    noise = 0
+
+if add_bias == 1:
+    bias = 20 # This value will differ
+else:
+    bias = 0
 
 
 
