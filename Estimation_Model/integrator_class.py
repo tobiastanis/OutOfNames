2754c60aad800503154ba8dda51f4576495a7e40
tobiastanis <t.p.tanis@student.tudelft.nl>
2022-06-07 23:09:39 +0200

    MEasurements and erros are zero atm
    Running another nominal trajectory

--- Estimation_Model/integrator_class.py
+++ Estimation_Model/integrator_class.py
@@ -13,7 +13,55 @@ from tudatpy.kernel.numerical_simulation import propagation, propagation_setup
 from Satellites_list.ELO import ELO
 from Satellites_list.EML2O import EML2O
 
-class EstimationClass:
+class EstimationClass_eml2o:
+    def __init__(self, name, mass, Aref, Cr, t0, tend, dt):
+        self.name = name
+        self.mass = mass
+        self.Aref = Aref
+        self.Cr = Cr
+        self.body_to_propagate = [self.name]
+        self.t0 = t0
+        self.tend = tend
+        self.dt = dt
+        self.bodies = None
+        self.acceleration_models = None
+
+        spice.load_standard_kernels()
+
+    def create_variables(self):
+        bodies_to_create = ["Earth", "Moon", "Sun", "Jupiter"]
+        #body_settings = environment_setup.get_default_body_settings(bodies_to_create, "Earth", "J2000")
+        body_settings = environment_setup.get_default_body_settings_time_limited(
+            bodies_to_create, self.t0, self.tend, "Earth", "J2000", self.dt
+        )
+        self.bodies = environment_setup.create_system_of_bodies(body_settings)
+        self.central_bodies = ["Earth"]
+
+        self.bodies.create_empty_body(self.name)
+        self.bodies.get(self.name).mass = self.mass
+
+        radiation_pressure_settings = environment_setup.radiation_pressure.cannonball(
+            "Sun", self.Aref, self.Cr
+        )
+
+        environment_setup.add_radiation_pressure_interface(self.bodies, self.name, radiation_pressure_settings)
+
+        acceleration_settings = dict(
+            Earth=[propagation_setup.acceleration.point_mass_gravity()],
+            Moon=[propagation_setup.acceleration.point_mass_gravity()],
+            Sun=[propagation_setup.acceleration.point_mass_gravity(),
+                 propagation_setup.acceleration.cannonball_radiation_pressure()],
+            Jupiter=[propagation_setup.acceleration.point_mass_gravity()],
+        )
+
+        acceleration_settings = {
+            self.name: acceleration_settings
+        }
+
+        self.acceleration_models = propagation_setup.create_acceleration_models(
+            self.bodies, acceleration_settings, self.body_to_propagate, self.central_bodies)
+
+class EstimationClass_elo:
     def __init__(self, name, mass, Aref, Cr, occulting_bodies, t0, tend, dt):
         self.name = name
         self.mass = mass
@@ -30,7 +78,7 @@ class EstimationClass:
         spice.load_standard_kernels()
 
     def create_variables(self):
-        bodies_to_create = ["Earth", "Moon", "Sun", "Jupiter", "Venus", "Mercury", "Saturn" , "Mars"]
+        bodies_to_create = ["Earth", "Moon", "Sun", "Jupiter", "Venus", "Mercury", "Saturn" , "Mars", "Uranus", "Neptune"]
         #body_settings = environment_setup.get_default_body_settings(bodies_to_create, "Earth", "J2000")
         body_settings = environment_setup.get_default_body_settings_time_limited(
             bodies_to_create, self.t0, self.tend, "Earth", "J2000", self.dt
@@ -48,15 +96,17 @@ class EstimationClass:
         environment_setup.add_radiation_pressure_interface(self.bodies, self.name, radiation_pressure_settings)
 
         acceleration_settings = dict(
-            Earth=[propagation_setup.acceleration.point_mass_gravity()],
-            Moon=[propagation_setup.acceleration.spherical_harmonic_gravity(12, 12)],
+            Earth=[propagation_setup.acceleration.spherical_harmonic_gravity(5, 5)],
+            Moon=[propagation_setup.acceleration.spherical_harmonic_gravity(30, 30)],
             Sun=[propagation_setup.acceleration.point_mass_gravity(),
                  propagation_setup.acceleration.cannonball_radiation_pressure()],
             Jupiter=[propagation_setup.acceleration.point_mass_gravity()],
             Venus=[propagation_setup.acceleration.point_mass_gravity()],
             Mars=[propagation_setup.acceleration.point_mass_gravity()],
             Mercury=[propagation_setup.acceleration.point_mass_gravity()],
-            Saturn=[propagation_setup.acceleration.point_mass_gravity()]
+            Saturn=[propagation_setup.acceleration.point_mass_gravity()],
+            Uranus=[propagation_setup.acceleration.point_mass_gravity()],
+            Neptune=[propagation_setup.acceleration.point_mass_gravity()]
         )
 
         acceleration_settings = {
@@ -65,7 +115,5 @@ class EstimationClass:
 
         self.acceleration_models = propagation_setup.create_acceleration_models(
             self.bodies, acceleration_settings, self.body_to_propagate, self.central_bodies)
-
-
 
 
--- figures_scripts/Estimation_Simulation_figures.py
+++ figures_scripts/Estimation_Simulation_figures.py
@@ -3,21 +3,64 @@ from Estimation_Model.Executor import stdP
 from Estimation_Model.Executor import t
 from Estimation_Model.Executor import x_error
 from Estimation_Model.Executor import visibility
-
+from Estimation_Model.Executor import X
+from Measurement_Model.Nominal_Observations_Cooker import states
 
 std_Pk_up = 3*stdP
 std_Pk_down =-3*stdP
+
+print("elo pos error:", x_error[0:10, :])
+
+
+fig1, (ax1, ax2, ax3) = plt.subplots(3, 1, constrained_layout=True, sharey=True)
+ax1.plot(t, states[:, 0], color='blue', label='Nominal')
+ax1.plot(t, X[:, 0], color='red', label='Estimated')
+ax2.plot(t, states[:, 1], color='blue')
+ax2.plot(t, X[:, 1], color='red')
+ax3.plot(t, states[:, 2], color='blue')
+ax3.plot(t, X[:, 2], color='red')
+ax1.legend()
+
+fig2, (ax1, ax2, ax3) = plt.subplots(3, 1, constrained_layout=True, sharey=True)
+ax1.plot(t, states[:, 3], color='blue', label='Nominal')
+ax1.plot(t, X[:, 3], color='red', label='Estimated')
+ax2.plot(t, states[:, 4], color='blue')
+ax2.plot(t, X[:, 4], color='red')
+ax3.plot(t, states[:, 5], color='blue')
+ax3.plot(t, X[:, 5], color='red')
+ax1.legend()
+
+fig3, (ax1, ax2, ax3) = plt.subplots(3, 1, constrained_layout=True, sharey=True)
+ax1.plot(t, states[:, 6], color='blue', label='Nominal')
+ax1.plot(t, X[:, 6], color='red', label='Estimated')
+ax2.plot(t, states[:, 7], color='blue')
+ax2.plot(t, X[:, 7], color='red')
+ax3.plot(t, states[:, 8], color='blue')
+ax3.plot(t, X[:, 8], color='red')
+ax1.legend()
+
+fig4, (ax1, ax2, ax3) = plt.subplots(3, 1, constrained_layout=True, sharey=True)
+ax1.plot(t, states[:, 9], color='blue', label='Nominal')
+ax1.plot(t, X[:, 9], color='red', label='Estimated')
+ax2.plot(t, states[:, 10], color='blue')
+ax2.plot(t, X[:, 10], color='red')
+ax3.plot(t, states[:, 11], color='blue')
+ax3.plot(t, X[:, 11], color='red')
+ax1.legend()
+
 
 plt.figure()
 plt.plot(t, x_error[:, 0], color='red', label='x')
 plt.plot(t, x_error[:, 1], color='blue', label='y')
 plt.plot(t, x_error[:, 2], color='green', label='z')
+"""
 plt.plot(t, std_Pk_up[:, 0], color='orange', linestyle='--' , label='3$\sigma_{x}$')
 plt.plot(t, std_Pk_down[:, 0], color='orange', linestyle='--')
 plt.plot(t, std_Pk_up[:, 1], color='cyan', linestyle='--', label='3$\sigma_{y}$')
 plt.plot(t, std_Pk_down[:, 1], color='cyan', linestyle='--')
 plt.plot(t, std_Pk_up[:, 2], color='yellow', linestyle='--', label='3$\sigma_{z}$')
 plt.plot(t, std_Pk_down[:, 2], color='yellow', linestyle='--')
+"""
 plt.legend()
 plt.xlim(0, 10)
 plt.grid(True, which="both", ls="-")
@@ -29,12 +72,14 @@ plt.figure()
 plt.plot(t, x_error[:, 3], color='red', label='$\dot{x}$')
 plt.plot(t, x_error[:, 4], color='blue', label='$\dot{y}$')
 plt.plot(t, x_error[:, 5], color='green', label='$\dot{z}$')
+"""
 plt.plot(t, std_Pk_up[:, 3], color='orange', linestyle='--', label='3$\sigma_{\dot{x}}$')
 plt.plot(t, std_Pk_down[:, 3], color='orange', linestyle='--')
 plt.plot(t, std_Pk_up[:, 4], color='cyan', linestyle='--', label='3$\sigma_{\dot{y}}$')
 plt.plot(t, std_Pk_down[:, 4], color='cyan', linestyle='--')
 plt.plot(t, std_Pk_up[:, 5], color='yellow', linestyle='--', label='3$\sigma_{\dot{z}}$')
 plt.plot(t, std_Pk_down[:, 5], color='yellow', linestyle='--')
+"""
 plt.legend()
 plt.xlim(0, 10)
 plt.grid(True, which="both", ls="-")
@@ -46,12 +91,14 @@ plt.figure()
 plt.plot(t, x_error[:, 6], color='red', label='x')
 plt.plot(t, x_error[:, 7], color='blue', label='y')
 plt.plot(t, x_error[:, 8], color='green', label='z')
+"""
 plt.plot(t, std_Pk_up[:, 6], color='orange', linestyle='--', label='3$\sigma_{x}$')
 plt.plot(t, std_Pk_down[:, 6], color='orange', linestyle='--')
 plt.plot(t, std_Pk_up[:, 7], color='cyan', linestyle='--', label='3$\sigma_{y}$')
 plt.plot(t, std_Pk_down[:, 7], color='cyan', linestyle='--')
 plt.plot(t, std_Pk_up[:, 8], color='yellow', linestyle='--', label='3$\sigma_{z}$')
 plt.plot(t, std_Pk_down[:, 8], color='yellow', linestyle='--')
+"""
 plt.legend()
 plt.xlim(0, 10)
 plt.grid(True, which="both", ls="-")
@@ -63,12 +110,14 @@ plt.figure()
 plt.plot(t, x_error[:, 9], color='red', label='$\dot{x}$')
 plt.plot(t, x_error[:, 10], color='blue', label='$\dot{y}$')
 plt.plot(t, x_error[:, 11], color='green', label='$\dot{z}$')
+"""
 plt.plot(t, std_Pk_up[:, 9], color='orange', linestyle='--', label='3$\sigma_{\dot{x}}$')
 plt.plot(t, std_Pk_down[:, 9], color='orange', linestyle='--')
 plt.plot(t, std_Pk_up[:, 10], color='cyan', linestyle='--', label='3$\sigma_{\dot{y}}$')
 plt.plot(t, std_Pk_down[:, 10], color='cyan', linestyle='--')
 plt.plot(t, std_Pk_up[:, 11], color='yellow', linestyle='--', label='3$\sigma_{\dot{z}}$')
 plt.plot(t, std_Pk_down[:, 11], color='yellow', linestyle='--')
+"""
 plt.legend()
 plt.xlim(0, 10)
 plt.grid(True, which="both", ls="-")
